b85691c8ddc446602cd6e452d4de9348aaaf2bee
Kakon Ray <thisiskakonray@gmail.com>
2022-05-02 21:28:56 +0600

    responsive

--- src/component/Blog/Blog.js
+++ src/component/Blog/Blog.js
@@ -6,7 +6,7 @@ const Blog = () => {
     <>
       <PageBanner page="Blog"></PageBanner>
       <div className="container mx-auto px-4 md:px-20 my-30 mb-24 mt-14">
-        <div className="grid grid-cols-3 gap-4">
+        <div className="grid md:grid-cols-3 gap-4">
           <div className="flex justify-center">
             <div className="rounded-lg shadow-lg bg-white max-w-sm">
               <a href="#!" data-mdb-ripple="true" data-mdb-ripple-color="light">

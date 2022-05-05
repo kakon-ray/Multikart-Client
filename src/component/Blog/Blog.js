e29b9072a384c55c7ddc101fb64429647ca505f7
Kakon Ray <thisiskakonray@gmail.com>
2022-05-05 20:19:48 +0600

    home page section add

--- src/component/Blog/Blog.js
+++ src/component/Blog/Blog.js
@@ -18,11 +18,19 @@ const Blog = () => {
               </a>
               <div className="p-6">
                 <h5 className="text-gray-900 text-xl font-medium mb-2">
-                  Card title
+                  জাভাস্ক্রিপ্ট এবং Node.js এর মধ্যে পার্থক্য
                 </h5>
                 <p className="text-gray-700 text-base mb-4">
-                  Some quick example text to build on the card title and make up
-                  the bulk of the card's content.
+                  জাভাস্ক্রিপ্ট একটি প্রোগ্রামিং ল্যাঙ্গুয়েজ।আর নোড জেস হচ্চে
+                  এই জাভাস্ক্রিপ্ট কে ব্যাকএন্ড এ চালানোর রানটাইম ইনভায়রমেন্ট যা
+                  ব্রাউজারের বাইরে জাভাস্ক্রিপ্ট কে কম্পাইল করে থাকে। নোড আসার
+                  আগে জাভাস্ক্রিপ্ট কে শুধু ব্রাউজারে রান করা যেত। জাভাস্ক্রিপ্ট
+                  ব্রাউজারে রান হত ক্রোম বা V8 ইঞ্জিন দ্বারা। Ryan Dahl নামের
+                  একজন ব্যক্তি ক্রোম এর' V8 ইঞ্জিন এবং সি ++ এর মাধ্যমে নোড জেস
+                  তৈরী করেন।এই নোড জেস এর মাধ্যমে জাভাস্ক্রিপ্ট কে সারর্ভার
+                  সাইডে রান করা সম্ভব হচ্চে। তাই বলা যায় জাভাস্ক্রিপ্ট একটি
+                  প্রোগ্রামিং ল্যাঙ্গুয়েজ এবং node.js ইনভারমেন্ট আসার পর একটি
+                  শক্তিশালী জনপ্রিয় প্রোগ্রামিং ল্যাংগুয়েজের পরিণত হয়েছে
                 </p>
                 <button
                   type="button"
@@ -44,11 +52,50 @@ const Blog = () => {
               </a>
               <div className="p-6">
                 <h5 className="text-gray-900 text-xl font-medium mb-2">
-                  Card title
+                  কখন নোড জেস ব্যবহার করা উচিত ?কখন মঙ্গডিবি ব্যাবহার করা উচিত?
                 </h5>
                 <p className="text-gray-700 text-base mb-4">
-                  Some quick example text to build on the card title and make up
-                  the bulk of the card's content.
+                  নোড জেস হাই পারফর্ম জাভাস্ক্রিপ্ট রান টাইম এনভায়রমেন্ট। কারণ
+                  জাভাস্ক্রিপট একটি এসিনক্রোনাস প্রোগ্রামিং ল্যাঙ্গুয়েজ। আর নোড
+                  জেস ও আসিনক্রোনাস ওয়েতে কাজ করে। একারণে অন্যান্য সিনক্রোনাস
+                  প্রোগ্রামিং ল্যাঙ্গুয়েজ এর তুলনায় নোড জেস থেকে খুব ভালো
+                  পারফর্মেন্স পাওয়া যায়। তাই বলা যায় ছোট থেকে মাঝারি এবং অনেক বড়
+                  এপ্লিকেশনে নোড জেস ব্যবহার করে ভালো পারফর্মেন্স পাওয়া যায়।
+                  মঙ্গডিবি একটি nosql database .এখানে ডাটা ডকুমেন্ট আকারে সাজানো
+                  থাকে।তাই যেসব এপ্লিকেশনে ডেটা ডকুমেন্টেশন আকারে সাজানো প্রয়োজন
+                  সেসব এপ্লিকেশনে mongodb ব্যবহার করা উচিত।
+                </p>
+                <button
+                  type="button"
+                  className=" inline-block px-6 py-2.5 bg-purple-600 text-white font-medium text-xs leading-tight uppercase rounded shadow-md hover:bg-purple-700 hover:shadow-lg focus:bg-purple-700 focus:shadow-lg focus:outline-none focus:ring-0 active:bg-blue-800 active:shadow-lg transition duration-150 ease-in-out"
+                >
+                  Details
+                </button>
+              </div>
+            </div>
+          </div>
+          <div className="flex justify-center">
+            <div className="rounded-lg shadow-lg bg-white max-w-sm">
+              <a href="#!" data-mdb-ripple="true" data-mdb-ripple-color="light">
+                <img
+                  className="rounded-t-lg"
+                  src="https://mdbootstrap.com/img/new/standard/nature/182.jpg"
+                  alt=""
+                />
+              </a>
+              <div className="p-6">
+                <h5 className="text-gray-900 text-xl font-medium mb-8">
+                  sql এবং nosql এর মধ্যে পার্থক্যঃ
+                </h5>
+                <p className="text-gray-700 text-base mb-16">
+                  sql একটি টেবিল বেস ডাটাবেস অপরদিকে nosql একটি ডকুমেন্ট বেশ
+                  ডাটাবেস। sql এর ডাটাগুলো টেবিল আকারে সাজানো থাকে অপরদিকে nosql
+                  এ ডাটা জাভাস্ক্রিপ্ট এর ডকুমেন্ট আকারে সাজানো থাকে। sql থেকে
+                  nosql ডাটাবেস বেশি ডেভেলপার ফ্রেন্ডলি।nosql অপারেশন গুলা অনেক
+                  সহজে করা যায়।mongodb একটি nosql ডাটাবেস। অপরদিকে mysql একটি
+                  sql ডাটাবেস। sql ডাটাবেস কে কুয়েরি এর মাধ্যমে ডাটা এর অপারেশন
+                  করতে হয়। অপরদিকে nosql ডাটাবেস কে প্রোগ্রামিং ল্যাংগুয়েজ যেমন
+                  জাভাস্ক্রিপ্ট php এর মাধ্যমে নিয়ন্ত্রণ করা যায়।
                 </p>
                 <button
                   type="button"
@@ -70,37 +117,12 @@ const Blog = () => {
               </a>
               <div className="p-6">
                 <h5 className="text-gray-900 text-xl font-medium mb-2">
-                  Card title
+                  What is JWT
                 </h5>
                 <p className="text-gray-700 text-base mb-4">
-                  Some quick example text to build on the card title and make up
-                  the bulk of the card's content.
-                </p>
-                <button
-                  type="button"
-                  className=" inline-block px-6 py-2.5 bg-purple-600 text-white font-medium text-xs leading-tight uppercase rounded shadow-md hover:bg-purple-700 hover:shadow-lg focus:bg-purple-700 focus:shadow-lg focus:outline-none focus:ring-0 active:bg-blue-800 active:shadow-lg transition duration-150 ease-in-out"
-                >
-                  Details
-                </button>
-              </div>
-            </div>
-          </div>
-          <div className="flex justify-center">
-            <div className="rounded-lg shadow-lg bg-white max-w-sm">
-              <a href="#!" data-mdb-ripple="true" data-mdb-ripple-color="light">
-                <img
-                  className="rounded-t-lg"
-                  src="https://mdbootstrap.com/img/new/standard/nature/182.jpg"
-                  alt=""
-                />
-              </a>
-              <div className="p-6">
-                <h5 className="text-gray-900 text-xl font-medium mb-2">
-                  Card title
-                </h5>
-                <p className="text-gray-700 text-base mb-4">
-                  Some quick example text to build on the card title and make up
-                  the bulk of the card's content.
+                  JWT means Json Web Token.JWT টোকেন এর মাধ্যমে ইউজার এর
+                  পার্সোনাল ডাটাগুলো সিকিউর করা যায়। অর্থাৎ উজাড় যখন সাইটে লগইন
+                  থাকবে তক্ষন ইউজার ডাটাগুলো একসেস করতে পারবে।{" "}
                 </p>
                 <button
                   type="button"
--- src/component/Pages/Home/Home.js
+++ src/component/Pages/Home/Home.js
@@ -7,6 +7,8 @@ import Product from "../../Product/Product";
 import Button from "../../Button";
 import Loading from "../../Loading/Loading";
 import Zoom from "react-reveal/Zoom";
+import Fade from "react-reveal/Fade";
+
 const Home = () => {
   const [products, setProducts] = useProducts();
   const homeProdcuts = products.slice(0, 6);
@@ -14,13 +16,11 @@ const Home = () => {
   return (
     <>
       <Slider></Slider>
-      <Delivery></Delivery>
 
       <div className="container mx-auto px-4 md:px-20 my-30 my-24">
         <h1 className="text-3xl text-stone-900 font-bold uppercase text-center mx-auto mb-12">
           Stock Products
         </h1>
-
         {products.length === 0 ? (
           <div className="flex justify-center items-center space-x-2">
             <div
@@ -57,6 +57,58 @@ const Home = () => {
         {/* <div className="flex justify-center mt-12">
           <Button>Update All Inventory</Button>
         </div> */}
+      </div>
+      <Delivery></Delivery>
+      <div
+        className="grid md:grid-cols-2 sm:grid-cols-2 xl:grid-cols-3  gap-4 h-100 my-36"
+        style={{
+          backgroundImage:
+            "url(" + "https://i.ibb.co/Rjn9grS/bg-image-1.jpg" + ")",
+          backgroundPosition: "center",
+          backgroundSize: "cover",
+          backgroundRepeat: "no-repeat",
+        }}
+      >
+        <div></div>
+
+        <div className="col-span-2 py-12 bg-lime-600 text-gray-300 bg-opacity-80 px-12 skew-y-12">
+          <Fade right>
+            <div className="grid xl:grid-cols-2 md:grid-cols-2 gap-8 my-5">
+              <div>
+                <h1 className="text-xl">Fast Delivery</h1>
+                <p>
+                  Nam liber tempor cum soluta nobis eleifend option congue nihil
+                  imperdiet doming id quod mazim placerat facer possim assum.
+                </p>
+              </div>
+              <div>
+                <h1 className="text-xl">Secure Packaging</h1>
+                <p>
+                  Nam liber tempor cum soluta nobis eleifend option congue nihil
+                  imperdiet doming id quod mazim placerat facer possim assum.
+                </p>
+              </div>
+            </div>
+          </Fade>
+          <Fade left>
+            <div className="grid xl:grid-cols-2 md:grid-cols-2 gap-8 my-8">
+              <div>
+                <h1 className="text-xl">Fast Delivery</h1>
+                <p>
+                  Nam liber tempor cum soluta nobis eleifend option congue nihil
+                  imperdiet doming id quod mazim placerat facer possim assum.
+                </p>
+              </div>
+              <div>
+                <h1 className="text-xl">Secure Packaging</h1>
+                <p>
+                  Nam liber tempor cum soluta nobis eleifend option congue nihil
+                  imperdiet doming id quod mazim placerat facer possim assum.
+                </p>
+              </div>
+            </div>
+          </Fade>
+        </div>
       </div>
 
       <MetOurTem></MetOurTem>
